0681771eb84a97d63838ae0c156e496ce6d59329
13426151793 <136502993@qq.com>
2025-12-14 21:03:52 +0800

    Update author name and add async call to tool
    update

--- client1.py
+++ client1.py
@@ -1,4 +1,17 @@
 # -*- coding: utf-8 -*-
 # @Time : 2025/12/14 19:58
-# @Author : jiangzhijie
+# @Author : tmac
 # @File : client1.py
+import asyncio
+
+from fastmcp import Client
+
+client = Client("http://127.0.0.1:8000/mcp")
+
+async def call_tool(name:str):
+    async with client:
+        response = await client.call_tool("great",{"name":name})
+        print(response)
+
+
+asyncio.run(call_tool("TMAC"))
