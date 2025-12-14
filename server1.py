8d132d6accaa7b38e7d99ed77b0889012e8d2e0e
13426151793 <136502993@qq.com>
2025-12-14 21:04:18 +0800

    Update server1.py
    update server

--- server1.py
+++ server1.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
 # @Time : 2025/12/14 19:40
-# @Author : jiangzhijie
+# @Author : tmac
 # @File : server1.py
+from fastmcp import FastMCP
+mcp = FastMCP("first_mcp_server")
+
+@mcp.tool
+def great(name: str)->str:
+    return f"Hello {name}, 欢迎来到我的第一个MCP服务!"
+
+if __name__ == "__main__":
+    mcp.run(transport="http", host="127.0.0.1", port=8000)
